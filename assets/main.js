7ede8cd79ac7a7124f2491b172255a23bf8dfe7c
jappu2 <jaber.fallatah@gmail.com>
2023-01-04 22:28:55 +0300

    mobile version first commit

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+
+document.querySelector(".menu-icon").addEventListener('click', function toggleMenu(){
+    let menu = document.querySelector('.navigation')
+     menu.classList.toggle('hidden')
+     this.classList.toggle("opend")
+
+})
