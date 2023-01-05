9bc8d967af736d74ad4bf5c6ea33d0cf0d6333c3
jappu2 <jaber.fallatah@gmail.com>
2023-01-05 11:18:05 +0300

    mobile version - navigation enhanced

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,9 @@
 
 document.querySelector(".menu-icon").addEventListener('click', function toggleMenu(){
-    let menu = document.querySelector('.navigation')
+    let menu = document.querySelector('.navigation'),
+    shadeLayer = document.querySelector('.shade-layer')
      menu.classList.toggle('hidden')
+     shadeLayer.classList.toggle('hidden')
      this.classList.toggle("opend")
 
 })
